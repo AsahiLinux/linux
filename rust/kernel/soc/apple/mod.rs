35a3e8fd021639671489459843b06cbb101e1c1c
arceuss <tavyntaplin@gmail.com>
2024-08-09 04:01:26 -0600

    This code very caver

--- rust/kernel/soc/apple/mod.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-only OR MIT
-
-//! Apple SoC drivers
-
-#[cfg(CONFIG_APPLE_RTKIT = "y")]
-pub mod rtkit;
--- rust/kernel/soc/mod.rs
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0
-
-//! SoC drivers
-
-pub mod apple;
